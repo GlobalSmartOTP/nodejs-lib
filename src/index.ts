da690d0221b17f461bd5f763f10bcb18fefbc825
mojtaba ghasemzadeh <mqtstbh@gmail.com>
2021-08-08 09:02:32 +0430

    ساده‌سازی

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import got from 'got'
+import { parse, stringify } from './jsonBigInt'
 import { SendInput, SendManualyInput, SendAutomaticInput, OTPSendResponse, VerifyRequest, OTPStatusRequest, OTPStatusResponse, OTPSendRequest } from './types'
 export { SendInput, SendManualyInput, SendAutomaticInput, OTPSendResponse, VerifyRequest, OTPStatusRequest, OTPStatusResponse, OTPSendRequest }
 export { OTPMethod, OTPStatus } from './types'
@@ -9,7 +10,7 @@ export class GsOTP<IsManual extends boolean = false> {
   /**
    * @param apiKey Your API key in gsOTP.com.
    * @param manual Generate OTP code automatically or you gonna to enter it manually. <br>Default is `false`.
-   * @param language Target language for error messages.
+   * @param language Target language for showing error messages.
    */
   constructor(
     private readonly apiKey: string,
@@ -51,20 +52,13 @@ export class GsOTP<IsManual extends boolean = false> {
       })
   }
 
-  private send(method: OTPSendRequest['method'], options: SendInput<IsManual>): Promise<bigint> {
-    const body: OTPSendRequest = {
+  private send(method: OTPSendRequest['method'], options: SendInput<IsManual>): Promise<bigint> { 
+    const body = {
       method,
-      countryCode: options.countryCode,
-      mobile: options.mobile,
       smart: false, // not implemented in core.gsotp.com yet!
-      param1: options.param1,
-      param2: options.param2,
-      param3: options.param3,
+      ...options,
       expireTime: options.expireTime ? +options.expireTime : undefined,
-      templateID: options.templateID,
-      code: (options as SendManualyInput).code,
-      length: (options as SendAutomaticInput).length,
-    }
+    } as OTPSendRequest
 
     return this.request<OTPSendResponse>('/otp/send', body)
       .then(result => result.referenceID)
@@ -98,12 +92,8 @@ export class GsOTP<IsManual extends boolean = false> {
    * @returns If the code is correct the promise resolves.
    */
   verify(options: VerifyRequest): Promise<void> {
-    const body: VerifyRequest = {
-      countryCode: options.countryCode,
-      mobile: options.mobile,
-      otp: options.otp,
-    }
-    return this.request<void>('/otp/verify', body)
+    return this.request<void>('/otp/verify', options)
+      .then(() => {})
   }
 
   /**
@@ -111,29 +101,6 @@ export class GsOTP<IsManual extends boolean = false> {
    * @param referenceID
    */
   getStatus(options: OTPStatusRequest): Promise<OTPStatusResponse> {
-    const body: OTPStatusRequest = {
-      OTPReferenceID: options.OTPReferenceID,
-    }
-    return this.request<OTPStatusResponse>('/otp/status', body)
+    return this.request<OTPStatusResponse>('/otp/status', options)
   }
-}
-
-function stringifyReplacer(key: string, value: any) {
-  return (typeof value === 'bigint') ? `BigInt(${String(value)})` : value
-}
-function stringify(object: any) {
-  let string = JSON.stringify(object, stringifyReplacer)
-  string = string.replace(/"BigInt\((\d+)\)"/g, '$1')
-  return string
-}
-
-function jsonParseReviver(key: string, value: any) {
-  if (typeof value === 'string' && /^BigInt\(\d+\)$/.test(value)) {
-    return BigInt(value.match(/\d+/)![0])
-  }
-  return value
-}
-function parse(string: any) {
-  string = string.replace(/\:\s*(\d{15}\d+)\s*([,}])/g, ':"BigInt($1)"$2')
-  return JSON.parse(string, jsonParseReviver)
 }
